8ea8d0013b695c66c3858fe75662fa44acbf0d5b
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-26 13:16:25 +0700

    add home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/Hedader/Header';
+import Home from './components/Home/Home';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className="main">
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div>
+            <div className="home__social">
+                <a href="" className="home__social-icon" target="_blank">
+                    <i class="uil uil-instagram-alt"></i>
+                </a>
+
+                <a href="" className="home__social-icon" target="_blank">
+                    <i class="uil uil-github"></i>
+                </a>
+
+                <a href="" className="home__social-icon" target="_blank">
+                    <i class="uil uil-gitlab"></i>
+                </a>
+
+            </div>
+        </div>
+    )
+}
+
+export default Social
