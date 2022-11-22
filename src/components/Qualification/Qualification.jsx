90dfedcaacbdc282b8021c4928386be31d02c607
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-22 14:41:16 +0700

    update porto

--- src/components/Qualification/Qualification.jsx
+++ src/components/Qualification/Qualification.jsx
@@ -1,193 +1,55 @@
 import React from 'react';
 import './qualification.css';
 import porto1 from '../../assets/porto1.svg';
-import porto2 from '../../assets/porto2.png'
+import porto2 from '../../assets/porto2.png';
+import suit from '../../assets/suitjawa.png';
+import booksearch from '../../assets/booksearch.png';
+
 
 
 const Qualification = () => {
     return (
-        <section className="qualification section" id='portofolio'>
+        <div className="qualification">
             <h2 className="section__title">My Portofolio</h2>
-            <span className="section__subtitle">Project</span>       
-            {/* <div className="qualification__container container">
-                <div className="qualification__tabs">
-                    <div className="qualification__button qualification__active button--flex">
-                        <i className="uil uil-graduation-cap qualification__icon"></i>
-                        Education
-                    </div>
-
-                    <div className="qualification__button button--flex">
-                        <i className="uil uil-briefcase-alt qualification__icon"></i>
-                        Experience
-                    </div>
+            <span className="section__subtitle">Project</span>
+            <div className="qualification__porto">
+                <div className="porto__item">
+                    <img src={porto1} alt="" />
+                    <p>
+                        his is an online car rental website which can
+                        display data from the back-end API and can
+                        interact with costumers. This application that built
+                        with React.Js. As a customer in this application
+                        you can search the available car, determine the
+                        ate of renting, and make a payment. (<a href="https://binar-car-rental-platinum.netlify.app" target='blank'>view</a>)
+                    </p>
                 </div>
-
-                <div className="qualification__sections">
-                    <div className="qualification__content">
-                        <div className="qualification__data">
-                            <div>
-                                <h3 className="qualification__title">Web Design</h3>
-                                <span className="qualification__subtitle">UNY</span>
-                                <div className="qualification__calender">
-                                    <i className="uil uil-calender-alt"></i>
-                                    2017-2021
-                                </div>
-                            </div>
-
-                            <div>
-                                <span className="qualification__rounder"></span>
-                                <span className="qualification__line"></span>
-                            </div>
-                        </div>
-
-                        <div className="qualification__data">
-                            <div></div>
-
-                            <div>
-                                <span className="qualification__rounder"></span>
-                                <span className="qualification__line"></span>
-                            </div>
-
-                            <div>
-                                <h3 className="qualification__title">Art Director</h3>
-                                <span className="qualification__subtitle">UNY</span>
-                                <div className="qualification__calender">
-                                    <i className="uil uil-calender-alt"></i>
-                                    2017-2021
-                                </div>
-                            </div>
-
-
-                        </div>
-
-                        <div className="qualification__data">
-                            <div>
-                                <h3 className="qualification__title">Web Development</h3>
-                                <span className="qualification__subtitle">UNY</span>
-                                <div className="qualification__calender">
-                                    <i className="uil uil-calender-alt"></i>
-                                    2017-2021
-                                </div>
-                            </div>
-
-                            <div>
-                                <span className="qualification__rounder"></span>
-                                <span className="qualification__line"></span>
-                            </div>
-                        </div>
-
-                        <div className="qualification__data">
-                            <div></div>
-
-                            <div>
-                                <span className="qualification__rounder"></span>
-                                <span className="qualification__line"></span>
-                            </div>
-
-                            <div>
-                                <h3 className="qualification__title">UX</h3>
-                                <span className="qualification__subtitle">UNY</span>
-                                <div className="qualification__calender">
-                                    <i className="uil uil-calender-alt"></i>
-                                    2021-2022
-                                </div>
-                            </div>
-
-
-                        </div>
-
-                    </div>
+                <div className="porto__item">
+                    <img src={porto2} alt="" />
+                    <p>
+                        This application that built with React.Js. As a
+                        admin in this application you can see order details
+                        rented by costumers and there is a CRUD feature
+                        (create, read, update, delete) for data settings for
+                        cars to be rented. (<a href="https://binar-car-rental-admin.netlify.app" target='blank'>view</a>)
+                    </p>
                 </div>
-
-                <div className="qualification__sections">
-                    <div className="qualification__content">
-                        <div className="qualification__data">
-                            <div>
-                                <h3 className="qualification__title">Web Design</h3>
-                                <span className="qualification__subtitle">UNY</span>
-                                <div className="qualification__calender">
-                                    <i className="uil uil-calender-alt"></i>
-                                    2017-2021
-                                </div>
-                            </div>
-
-                            <div>
-                                <span className="qualification__rounder"></span>
-                                <span className="qualification__line"></span>
-                            </div>
-                        </div>
-
-                        <div className="qualification__data">
-                            <div></div>
-
-                            <div>
-                                <span className="qualification__rounder"></span>
-                                <span className="qualification__line"></span>
-                            </div>
-
-                            <div>
-                                <h3 className="qualification__title">Art Director</h3>
-                                <span className="qualification__subtitle">UNY</span>
-                                <div className="qualification__calender">
-                                    <i className="uil uil-calender-alt"></i>
-                                    2017-2021
-                                </div>
-                            </div>
-
-
-                        </div>
-
-                        <div className="qualification__data">
-                            <div>
-                                <h3 className="qualification__title">Web Development</h3>
-                                <span className="qualification__subtitle">UNY</span>
-                                <div className="qualification__calender">
-                                    <i className="uil uil-calender-alt"></i>
-                                    2017-2021
-                                </div>
-                            </div>
-
-                            <div>
-                                <span className="qualification__rounder"></span>
-                                <span className="qualification__line"></span>
-                            </div>
-                        </div>
-
-                    </div>
+                <div className="porto__item">
+                    <img src={suit} alt="" />
+                    <p>
+                    This application that built with React.Js. This game
+                    is played by pitting fingers against the computer to
+                    determine who wins. (<a href="https://suit-jawa-game.netlify.app/" target='blank'>view</a>)
+                    </p>
                 </div>
-
-            </div> */}
-
-            <div className="porto">
-                <div className="porto-container">
-                    <div className="porto-content">
-                        <img src={porto1} alt="" />
-                        <a href="https://binar-car-rental-platinum.netlify.app">
-                            <p>
-                                This is an online car rental website which can
-                                display data from the back-end API and can
-                                interact with costumers. This application that built
-                                with React.Js. As a customer in this application
-                                you can search the available car, determine the
-                                date of renting, and make a payment.
-                            </p>
-                        </a>
-                    </div>
-                    <div className="porto-content">
-                        <img src={porto2} alt="" />
-                        <a href="https://binar-car-rental-admin.netlify.app">
-                            <p>
-                                This application that built with React.Js. As a
-                                admin in this application you can see order details
-                                rented by costumers and there is a CRUD feature
-                                (create, read, update, delete) for data settings for
-                                cars to be rented.
-                            </p>
-                        </a>
-                    </div>
+                <div className="porto__item">
+                    <img src={booksearch} alt="" />
+                    <p>
+                    This application that built with React.Js. This application will assist users in searching for books and provide the required information. (<a href="https://cari-buku.netlify.app/" target='blank'>view</a>)
+                    </p>
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
